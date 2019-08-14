eb949b78bdf5966c42f3844a602d59e796685f03
Osusara Kammalawatta <30489601+osusara@users.noreply.github.com>
2019-08-14 21:38:37 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+	<link rel="stylesheet" type="text/css" href="css/reset.css">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
+	<link rel="stylesheet" type="text/css" href="css/responsive.css">
+
+	<title>GPA Calculator</title>
+</head>
+<body>
+
+	<div class="container-fluid">
+		<div class="row  align-middle">
+			<div class="col-md-4 col-sm-8 col-xs-10 mx-auto">
+				<div class="card">
+					<div class="card-body text-center">
+						<div class="topic">
+							<strong>GPA Calculator</strong>
+						</div>
+						<h6>Click Next begin</h6>
+					</div>
+				</div>
+			</div>
+		</div>
+		
+
+		<div class="container padding text-center">
+			<a class="btn btn-lg" href="pages/cis.php">Next</a>
+		</div>
+	</div>
+
+	<script type="text/javascript" src="js/functions.js"></script>
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>
